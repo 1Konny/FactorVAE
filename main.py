2ec8689b9a8239460080ca2f9b947deae66925ae
1Konny <lee.wonkwang94@gmail.com>
2018-06-16 01:21:52 +0900

    add output_save flag, and fix default type of iteration flags

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     parser.add_argument('--lr_VAE', default=1e-4, type=float, help='learning rate of the VAE')
     parser.add_argument('--beta1_VAE', default=0.9, type=float, help='beta1 parameter of the Adam optimizer for the VAE')
     parser.add_argument('--beta2_VAE', default=0.999, type=float, help='beta2 parameter of the Adam optimizer for the VAE')
-    parser.add_argument('--lr_D', default=1e-5, type=float, help='learning rate for training the discriminator')
+    parser.add_argument('--lr_D', default=1e-4, type=float, help='learning rate for training the discriminator')
     parser.add_argument('--beta1_D', default=0.5, type=float, help='beta1 parameter of the Adam optimizer for the discriminator')
     parser.add_argument('--beta2_D', default=0.9, type=float, help='beta2 parameter of the Adam optimizer for the discriminator')
 
@@ -45,19 +45,21 @@ if __name__ == "__main__":
     parser.add_argument('--image_size', default=64, type=int, help='image size. now only (64,64) is supported')
     parser.add_argument('--num_workers', default=2, type=int, help='dataloader num_workers')
 
-
     parser.add_argument('--viz_on', default=True, type=str2bool, help='enable visdom visualization')
-    parser.add_argument('--viz_port', default=8097, type=str, help='visdom port number')
-    parser.add_argument('--viz_ll_iter', default=1000, type=str, help='visdom line data logging iter')
-    parser.add_argument('--viz_la_iter', default=5000, type=str, help='visdom line data applying iter')
-    parser.add_argument('--viz_ra_iter', default=10000, type=str, help='visdom recon image applying iter')
-    parser.add_argument('--viz_ta_iter', default=10000, type=str, help='visdom traverse applying iter')
+    parser.add_argument('--viz_port', default=8097, type=int, help='visdom port number')
+    parser.add_argument('--viz_ll_iter', default=1000, type=int, help='visdom line data logging iter')
+    parser.add_argument('--viz_la_iter', default=5000, type=int, help='visdom line data applying iter')
+    parser.add_argument('--viz_ra_iter', default=10000, type=int, help='visdom recon image applying iter')
+    parser.add_argument('--viz_ta_iter', default=10000, type=int, help='visdom traverse applying iter')
 
-    parser.add_argument('--print_iter', default=500, type=str, help='print losse iter')
+    parser.add_argument('--print_iter', default=500, type=int, help='print losse iter')
 
     parser.add_argument('--ckpt_dir', default='checkpoints', type=str, help='checkpoint directory')
-    parser.add_argument('--ckpt_load', default=True, type=str2bool, help='load last checkpoint')
-    parser.add_argument('--ckpt_save_iter', default=5000, type=str, help='checkpoint save iter')
+    parser.add_argument('--ckpt_load', default=None, type=str, help='checkpoint name to load')
+    parser.add_argument('--ckpt_save_iter', default=5000, type=int, help='checkpoint save iter')
+
+    parser.add_argument('--output_dir', default='outputs', type=str, help='output directory')
+    parser.add_argument('--output_save', default=True, type=str2bool, help='whether to save traverse results')
 
     args = parser.parse_args()
 
