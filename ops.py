52195593245ad93e74aea5044c6a815e813f3ec8
1Konny <lee.wonkwang94@gmail.com>
2018-06-15 18:52:10 +0900

    split loss

--- ops.py
+++ ops.py
@@ -1,16 +1,16 @@
 import torch
 import torch.nn.functional as F
 
-def original_vae_loss(x, x_recon, mu, logvar):
-    batch_size = x.size(0)
-    if batch_size == 0:
-        recon_loss = 0
-        kl_divergence = 0
-    else:
-        recon_loss = F.binary_cross_entropy_with_logits(x_recon, x, size_average=False).div(batch_size)
-        kl_divergence = -0.5*(1 + logvar - mu**2 - logvar.exp()).sum(1).mean()
 
-    return recon_loss, kl_divergence
+def recon_loss(x, x_recon):
+    n = x.size(0)
+    recon_loss = F.binary_cross_entropy_with_logits(x_recon, x, size_average=False).div(n)
+    return recon_loss
+
+
+def kl_divergence(mu, logvar):
+    kld = -0.5*(1+logvar-mu**2-logvar.exp()).sum(1).mean()
+    return kld
 
 
 def permute_dims(z):
