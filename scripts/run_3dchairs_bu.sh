61967b69dc7ddfba3be6f07d5cb53477b2323dc9
1Konny <lee.wonkwang94@gmail.com>
2018-06-16 20:50:45 +0900

    make scripts directory

---
+++ scripts/run_3dchairs_bu.sh
@@ -0,0 +1,3 @@
+#1 /bin/bash
+
+python main.py --dataset 3dchairs --gamma 3.2 --viz_name factor_vae_3dchairs --viz_port 55558
---
+++ scripts/run_celeba_old.sh
@@ -0,0 +1,3 @@
+#1 /bin/bash
+
+python main.py --dataset celeba --gamma 6.4 --viz_name factor_vae_celeba --viz_port 55558
