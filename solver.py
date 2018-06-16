bc203192b5707c1826aa3096c80d00998bf9f445
1Konny <lee.wonkwang94@gmail.com>
2018-06-16 15:46:29 +0900

    add gif saving option in visualize_traverse. refactor variable names.

--- solver.py
+++ solver.py
@@ -2,15 +2,16 @@
 
 import time
 import os
+import random
 import visdom
+
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.autograd import Variable
-from torchvision.utils import make_grid
-from torchvision import transforms
+from torchvision.utils import make_grid, save_image
 
-from utils import cuda, mkdirs, DataGather
+from utils import DataGather, cuda, mkdirs, grid2gif
 from ops import recon_loss, kl_divergence, permute_dims
 from model import FactorVAE1, FactorVAE2, Discriminator
 from dataset import return_data
@@ -21,12 +22,13 @@ class Solver(object):
         # Misc
         self.use_cuda = args.cuda and torch.cuda.is_available()
         self.name = args.name
-        self.max_iter = args.max_iter
+        self.max_iter = int(args.max_iter)
         self.print_iter = args.print_iter
         self.global_iter = 0
 
         # Data
         self.dset_dir = args.dset_dir
+        self.dataset = args.dataset
         self.batch_size = args.batch_size
         self.data_loader = return_data(args)
 
@@ -44,8 +46,10 @@ class Solver(object):
 
         if args.dataset == 'dsprites':
             self.VAE = cuda(FactorVAE1(self.z_dim), self.use_cuda)
+            self.nc = 1
         else:
             self.VAE = cuda(FactorVAE2(self.z_dim), self.use_cuda)
+            self.nc = 3
         self.optim_VAE = optim.Adam(self.VAE.parameters(), lr=self.lr_VAE,
                                     betas=(self.beta1_VAE, self.beta2_VAE))
 
@@ -58,7 +62,7 @@ class Solver(object):
         # Visdom
         self.viz_on = args.viz_on
         self.win_id = dict(D_z='win_D_z', recon='win_recon', kld='win_kld', acc='win_acc')
-        self.line_gather = DataGather('iter', 'soft_D_z', 'soft_D_z_perm', 'recon', 'kld', 'acc')
+        self.line_gather = DataGather('iter', 'soft_D_z', 'soft_D_z_pperm', 'recon', 'kld', 'acc')
         self.image_gather = DataGather('true', 'recon')
         if self.viz_on:
             self.viz_port = args.viz_port
@@ -75,46 +79,45 @@ class Solver(object):
         self.ckpt_save_iter = args.ckpt_save_iter
         mkdirs(self.ckpt_dir)
         if args.ckpt_load:
-            self.load_checkpoint()
+            self.load_checkpoint(args.ckpt_load)
+
+        self.output_dir = os.path.join(args.output_dir, args.name)
+        self.output_save = args.output_save
+        mkdirs(self.output_dir)
 
     def train(self):
         self.net_mode(train=True)
 
-        ones = cuda(torch.ones(self.batch_size), self.use_cuda)
-        ones = Variable(ones)
-        zeros = cuda(torch.zeros(self.batch_size), self.use_cuda)
-        zeros = Variable(zeros)
+        ones = cuda(torch.ones(self.batch_size).long(), self.use_cuda)
+        ones = Variable(ones, volatile=True)
+        zeros = cuda(torch.zeros(self.batch_size).long(), self.use_cuda)
+        zeros = Variable(zeros, volatile=True)
 
         out = False
         while not out:
             start = time.time()
-            curve_data = []
-            for x_vae, x_disc in self.data_loader:
+            for x_true1, x_true2 in self.data_loader:
                 self.global_iter += 1
 
-                x_vae = Variable(cuda(x_vae, self.use_cuda))
-                x_recon, mu, logvar, z = self.VAE(x_vae)
-                vae_recon_loss = recon_loss(x_vae, x_recon)
+                x_true1 = Variable(cuda(x_true1, self.use_cuda))
+                x_recon, mu, logvar, z = self.VAE(x_true1)
+                vae_recon_loss = recon_loss(x_true1, x_recon)
                 vae_kld = kl_divergence(mu, logvar)
 
                 D_z = self.D(z)
-                #vae_tc_1 = F.binary_cross_entropy_with_logits(D_z, ones, -self.gamma)
-                #vae_tc_2 = F.binary_cross_entropy_with_logits(D_z, zeros, self.gamma)
-                #vae_tc_loss = vae_tc_1 + vae_tc_2
-                vae_tc_loss = D_z.mean()*self.gamma
+                vae_tc_loss = (D_z[:, :1] - D_z[:, 1:]).mean()
 
-                vae_loss = vae_recon_loss + vae_kld + vae_tc_loss
+                vae_loss = vae_recon_loss + vae_kld + self.gamma*vae_tc_loss
 
                 self.optim_VAE.zero_grad()
                 vae_loss.backward(retain_graph=True)
                 self.optim_VAE.step()
 
-                x_disc = Variable(cuda(x_disc, self.use_cuda))
-                z_prime = self.VAE(x_disc, no_dec=True)
-                z_perm = permute_dims(z_prime)
-                D_z_perm = self.D(z_perm.detach())
-                D_tc_loss = F.binary_cross_entropy_with_logits(
-                    torch.cat([D_z, D_z_perm]), torch.cat([ones, zeros]))
+                x_true2 = Variable(cuda(x_true2, self.use_cuda))
+                z_prime = self.VAE(x_true2, no_dec=True)
+                z_pperm = permute_dims(z_prime).detach()
+                D_z_pperm = self.D(z_pperm)
+                D_tc_loss = 0.5*(F.cross_entropy(D_z, zeros) + F.cross_entropy(D_z_pperm, ones))
 
                 self.optim_D.zero_grad()
                 D_tc_loss.backward()
@@ -128,23 +131,23 @@ class Solver(object):
                     self.save_checkpoint(self.global_iter)
 
                 if self.viz_on and (self.global_iter%self.viz_ll_iter == 0):
-                    soft_D_z = F.sigmoid(D_z)
-                    soft_D_z_perm = F.sigmoid(D_z_perm)
-                    disc_acc = ((soft_D_z >= 0.5).sum() + (soft_D_z_perm < 0.5).sum()).float()
-                    disc_acc /= 2*self.batch_size
+                    soft_D_z = F.softmax(D_z, 1)[:, :1].detach()
+                    soft_D_z_pperm = F.softmax(D_z_pperm, 1)[:, :1].detach()
+                    D_acc = ((soft_D_z >= 0.5).sum() + (soft_D_z_pperm < 0.5).sum()).float()
+                    D_acc /= 2*self.batch_size
                     self.line_gather.insert(iter=self.global_iter,
                                             soft_D_z=soft_D_z.mean().data[0],
-                                            soft_D_z_perm=soft_D_z_perm.mean().data[0],
+                                            soft_D_z_pperm=soft_D_z_pperm.mean().data[0],
                                             recon=vae_recon_loss.data[0],
                                             kld=vae_kld.data[0],
-                                            acc=disc_acc.data[0])
+                                            acc=D_acc.data[0])
 
                 if self.viz_on and (self.global_iter%self.viz_la_iter == 0):
                     self.visualize_line()
                     self.line_gather.flush()
 
                 if self.viz_on and (self.global_iter%self.viz_ra_iter == 0):
-                    self.image_gather.insert(true=x_vae.data.cpu(),
+                    self.image_gather.insert(true=x_true1.data.cpu(),
                                              recon=F.sigmoid(x_recon).data.cpu())
                     self.visualize_recon()
                     self.image_gather.flush()
@@ -176,10 +179,10 @@ class Solver(object):
         iters = torch.Tensor(data['iter'])
         recon = torch.Tensor(data['recon'])
         kld = torch.Tensor(data['kld'])
-        acc = torch.Tensor(data['acc'])
+        D_acc = torch.Tensor(data['acc'])
         soft_D_z = torch.Tensor(data['soft_D_z'])
-        soft_D_z_perm = torch.Tensor(data['soft_D_z_perm'])
-        soft_D_zs = torch.stack([soft_D_z, soft_D_z_perm], -1)
+        soft_D_z_pperm = torch.Tensor(data['soft_D_z_pperm'])
+        soft_D_zs = torch.stack([soft_D_z, soft_D_z_pperm], -1)
 
         self.viz.line(X=iters,
                       Y=soft_D_zs,
@@ -199,7 +202,7 @@ class Solver(object):
                         xlabel='iteration',
                         ylabel='reconstruction loss',))
         self.viz.line(X=iters,
-                      Y=acc,
+                      Y=D_acc,
                       env=self.name+'/lines',
                       win=self.win_id['acc'],
                       update='append',
@@ -215,39 +218,80 @@ class Solver(object):
                         xlabel='iteration',
                         ylabel='kl divergence',))
 
-    def visualize_traverse(self):
+    def visualize_traverse(self, limit=3, inter=2/3, loc=-1):
         self.net_mode(train=False)
+
         decoder = self.VAE.decode
         encoder = self.VAE.encode
-        interpolation = torch.arange(-6, 6.1, 1)
+        interpolation = torch.arange(-limit, limit+0.1, inter)
 
-        fixed_idx = 0
+        n_dsets = len(self.data_loader.dataset)
+        rand_idx = random.randint(1, n_dsets-1)
 
-        fixed_img, random_img = self.data_loader.dataset.__getitem__(fixed_idx)
-        fixed_img = Variable(cuda(fixed_img, self.use_cuda), volatile=True).unsqueeze(0)
-        fixed_img_z = encoder(fixed_img)[:, :self.z_dim]
-
+        random_img = self.data_loader.dataset.__getitem__(rand_idx)[1]
         random_img = Variable(cuda(random_img, self.use_cuda), volatile=True).unsqueeze(0)
         random_img_z = encoder(random_img)[:, :self.z_dim]
 
-        zero_z = Variable(cuda(torch.zeros(1, self.z_dim, 1, 1), self.use_cuda), volatile=True)
         random_z = Variable(cuda(torch.rand(1, self.z_dim, 1, 1), self.use_cuda), volatile=True)
 
-        Z = {'fixed_img':fixed_img_z, 'random_img':random_img_z, 'random_z':random_z, 'zero_z':zero_z}
-        for key in Z.keys():
+        if self.dataset == 'dsprites':
+            fixed_idx1 = 87040 # square
+            fixed_idx2 = 332800 # ellipse
+            fixed_idx3 = 578560 # heart
+
+            fixed_img1 = self.data_loader.dataset.__getitem__(fixed_idx1)[0]
+            fixed_img1 = Variable(cuda(fixed_img1, self.use_cuda), volatile=True).unsqueeze(0)
+            fixed_img_z1 = encoder(fixed_img1)[:, :self.z_dim]
+
+            fixed_img2 = self.data_loader.dataset.__getitem__(fixed_idx2)[0]
+            fixed_img2 = Variable(cuda(fixed_img2, self.use_cuda), volatile=True).unsqueeze(0)
+            fixed_img_z2 = encoder(fixed_img2)[:, :self.z_dim]
+
+            fixed_img3 = self.data_loader.dataset.__getitem__(fixed_idx3)[0]
+            fixed_img3 = Variable(cuda(fixed_img3, self.use_cuda), volatile=True).unsqueeze(0)
+            fixed_img_z3 = encoder(fixed_img3)[:, :self.z_dim]
+
+            Z = {'fixed_square':fixed_img_z1, 'fixed_ellipse':fixed_img_z2,
+                 'fixed_heart':fixed_img_z3, 'random_img':random_img_z}
+        else:
+            fixed_idx = 0
+            fixed_img = self.data_loader.dataset.__getitem__(fixed_idx)
+            fixed_img = Variable(cuda(fixed_img, self.use_cuda), volatile=True).unsqueeze(0)
+            fixed_img_z = encoder(fixed_img)[:, :self.z_dim]
+
+            Z = {'fixed_img':fixed_img_z, 'random_img':random_img_z, 'random_z':random_z}
+
+        gifs = []
+        for key in Z:
             z_ori = Z[key]
             samples = []
             for row in range(self.z_dim):
+                if loc != -1 and row != loc:
+                    continue
                 z = z_ori.clone()
                 for val in interpolation:
                     z[:, row] = val
-                    sample = F.sigmoid(decoder(z))
+                    sample = F.sigmoid(decoder(z)).data
                     samples.append(sample)
-            samples = torch.cat(samples, dim=0).data.cpu()
-            title = '{}_row_traverse(iter:{})'.format(key, self.global_iter)
-            self.viz.images(samples,
-                            env=self.name+'/traverse',
+                    gifs.append(sample)
+            samples = torch.cat(samples, dim=0).cpu()
+            title = '{}_latent_traversal(iter:{})'.format(key, self.global_iter)
+            self.viz.images(samples, env=self.name+'/traverse',
                             opts=dict(title=title), nrow=len(interpolation))
+
+        if self.output_save:
+            output_dir = os.path.join(self.output_dir, str(self.global_iter))
+            mkdirs(output_dir)
+            gifs = torch.cat(gifs)
+            gifs = gifs.view(len(Z), self.z_dim, len(interpolation), self.nc, 64, 64).transpose(1, 2)
+            for i, key in enumerate(Z.keys()):
+                for j, val in enumerate(interpolation):
+                    save_image(tensor=gifs[i][j].cpu(),
+                               filename=os.path.join(output_dir, '{}_{}.jpg'.format(key, j)),
+                               nrow=self.z_dim, pad_value=1)
+
+                grid2gif(str(os.path.join(output_dir, key+'*.jpg')),
+                         str(os.path.join(output_dir, key+'.gif')), delay=10)
 
         self.net_mode(train=True)
 
